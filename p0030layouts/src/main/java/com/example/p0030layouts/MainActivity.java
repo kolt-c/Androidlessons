84e858e221bb6d0fb9eec0cc130fbd6f5391fa48
Kolt-c <ts.nicolas@outlook.com>
2019-10-30 15:53:51 +0200

    lesson3 done

--- p0030layouts/src/main/java/com/example/p0030layouts/MainActivity.java
+++ p0030layouts/src/main/java/com/example/p0030layouts/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.androidlessons;
+package com.example.p0030layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.myscreen);
+        setContentView(R.layout.activity_main);
     }
 }
